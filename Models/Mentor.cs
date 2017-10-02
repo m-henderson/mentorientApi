fb416af33af51d535e5b742ccede0e57060e4372
Michael Henderson <mvhenderson.4@icloud.com>
2017-10-02 17:32:49 -0500

    add mentor model

---
+++ Models/Mentor.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace mentorient_api.Models
+{
+    public class Mentor
+    {
+        public int Id { get; set; }
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+        
+    }
+}
